fae0006b5a22cb1659925a6f0e702490c84ac347
Michael Herman <hermanmu@gmail.com>
2018-04-26 16:43:05 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class SimpleTest(unittest.TestCase):
+
+    def test(self):
+        num1 = 22
+        num2 = 22
+        self.assertEqual(num1, num2)
+
+
+if __name__ == '__main__':
+    unittest.main()
