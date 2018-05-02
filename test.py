2700f10bf80597acad50a2c7a996567ae8aa017f
Michael Herman <hermanmu@gmail.com>
2018-05-01 20:00:26 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,9 @@
-import time
 import unittest
 
 
 class SimpleTest(unittest.TestCase):
 
     def test(self):
-        time.sleep(10)
         num1 = 22
         num2 = 22
         self.assertEqual(num1, num2)
